2dccab39ec59b0386085a28b381db572f8f117f7
naumanalin <naumanalin865@gmail.com>
2025-03-03 00:51:48 +0500

    cors modifications

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const app = express()
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:5173", "https://yourfrontenddomain.com"],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }))
