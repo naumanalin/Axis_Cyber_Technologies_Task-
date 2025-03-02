d9a26281495a76a4a19280892cab47b64480e485
naumanalin <144068599+naumanalin@users.noreply.github.com>
2025-03-02 15:52:26 +0500

    Update cors config

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const app = express()
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:5173", "https://yourfrontenddomain.com"],
+    origin: ["http://localhost:5173", "https://budget-tracker-three-beta.vercel.app/", "http://localhost:5174"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }))
